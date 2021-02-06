dfc1234ddeaad48d26ae8cec50037d255f83bc8b
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-02-06 01:11:10 -0300

    se añaden botones e inputs

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -6,26 +6,33 @@ import {
   Table,
   Tbody,
   Tr,
+  Th,
   Td,
   Checkbox,
   Tooltip,
   Spinner,
-  PhoneIcon,
+  Tfoot,
+  Button,
+  Stack,
+  Input,
 } from "@chakra-ui/react";
 import { InfoOutlineIcon } from "@chakra-ui/icons";
 
 const Meals = () => {
   const [meals, setMeals] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [checkedMeals, setChekedMeals] = useState(null);
+  const [checkedMeals, setChekedMeals] = useState([]);
   const { setLayout, setErrorMessage } = useContext(Context);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     (async () => {
       setIsLoading(true);
 
       try {
-        const res = await axios.get("http://localhost:3001/api/meals");
+        const res = await axios.get(
+          "https://api-rest-gestmorfi.herokuapp.com/api/meals"
+        );
         setLayout("MAIN_PAGE");
         setMeals(res.data);
         setIsLoading(false);
@@ -36,9 +43,12 @@ const Meals = () => {
     })();
   }, []);
 
-  // const checkMeal = (e) => {
-  //   set
-  // };
+  const checkMeal = (e) => {
+    let currentCheckedMeals = [...checkedMeals];
+    let selectedMeal = meals.filter((meal) => meal._id == e.value);
+    console.log(selectedMeal);
+    // setChekedMeals(currentCheckedMeals);
+  };
 
   return (
     <>
@@ -56,36 +66,61 @@ const Meals = () => {
             />
           </div>
         ) : (
-          <Table
-            className="meals-list"
-            variant="striped"
-            colorScheme="blue"
-            size="sm"
-          >
-            <Tbody>
-              {meals &&
-                meals.map((meal) => (
-                  <Tr>
-                    <Td className="meal-title">{meal.name}</Td>
-                    <Td>
-                      <Tooltip label={meal.description} fontSize="md">
-                        <InfoOutlineIcon w={5} h={5} />
-                      </Tooltip>
-                    </Td>
-                    <Td isNumeric>$ {meal.price}</Td>
-                    <Td>
-                      <Checkbox
-                        size="lg"
-                        value={meal._id}
-                        className="check"
-                        colorScheme="green"
-                        onChange={(e) => checkMeal(e.target)}
-                      ></Checkbox>
-                    </Td>
-                  </Tr>
-                ))}
-            </Tbody>
-          </Table>
+          <>
+            <Table
+              className="meals-list"
+              variant="striped"
+              colorScheme="blue"
+              size="sm"
+            >
+              <Tbody>
+                {meals &&
+                  meals.map((meal) => (
+                    <Tr>
+                      <Td className="meal-title">{meal.name}</Td>
+                      <Td>
+                        <Tooltip label={meal.description} fontSize="md">
+                          <InfoOutlineIcon w={5} h={5} />
+                        </Tooltip>
+                      </Td>
+                      <Td isNumeric>$ {meal.price}</Td>
+                      <Td>
+                        <Checkbox
+                          size="lg"
+                          value={meal._id}
+                          className="check"
+                          colorScheme="green"
+                          onChange={(e) => checkMeal(e.target)}
+                        ></Checkbox>
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th className="total" pt={6}>
+                    Total
+                  </Th>
+                  <Th></Th>
+                  <Th className="total" isNumeric>
+                    $ {total}
+                  </Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+            <Stack className="stack" spacing={4} alignItems="center" pt={6}>
+              <Input variant="filled" placeholder="Adicionales" w="100%" />
+              <Input variant="filled" placeholder="Tu nombre" w="100%" />
+            </Stack>
+            <Button
+              className="confirm-button"
+              colorScheme="blue"
+              w="30%"
+              mt={8}
+            >
+              Confirmar
+            </Button>
+          </>
         )}
       </div>
     </>
