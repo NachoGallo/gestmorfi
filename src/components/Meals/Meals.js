8cf1402d0bcdef86fcdfc474d79cac8ee8bd5162
Nacho Gallo <nacho.r.gallo@gmail.com>
2021-02-09 19:06:34 -0300

    Se agrega submit meals

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import axios from "axios";
 import "./Meals.css";
-import { validateCheckbox, SendToast } from "../../utils/utilsFunctions";
+import {
+  validateCheckbox,
+  ShowToast,
+  cleanData,
+} from "../../utils/utilsFunctions";
 import { Context } from "../../context/Context";
 import {
   Table,
   Tbody,
   Tr,
-  Th,
   Td,
   Checkbox,
   Tooltip,
   Spinner,
-  Tfoot,
   Button,
   Stack,
   Input,
-  useToast,
+  Flex,
+  Spacer,
+  Box,
 } from "@chakra-ui/react";
 import { InfoOutlineIcon } from "@chakra-ui/icons";
 
@@ -24,10 +28,15 @@ const Meals = () => {
   const [meals, setMeals] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [buttonLoading, setButtonLoading] = useState(false);
-  const { setLayout, setErrorMessage } = useContext(Context);
   const [total, setTotal] = useState(0);
+  const [invalidInput, setInvalidInput] = useState(false);
+  const { setLayout, setErrorMessage } = useContext(Context);
+  const [orderData, setOrderData] = useState({ name: "", additional: "" });
+  const [uncheck, setUncheck] = useState(false);
+
   const checkboxRef = useRef([]);
-  const toast = useToast();
+  const inputNameRef = useRef(null);
+  const inputAddRef = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -47,11 +56,11 @@ const Meals = () => {
     })();
   }, []);
 
-  const checkMeal = () => {
+  const generateOrder = async () => {
     setButtonLoading(true);
     const oneChecked = validateCheckbox(checkboxRef);
     if (!oneChecked) {
-      SendToast(
+      ShowToast(
         "error",
         "Error al realizar pedido.",
         "Debe seleccionar al menos una comida para realizar el pedido."
@@ -59,17 +68,53 @@ const Meals = () => {
       setButtonLoading(false);
       return;
     }
+
+    const orderPayload = {
+      mealId: oneChecked,
+      userId: orderData.name,
+      additional: orderData.additional,
+      price: total,
+    };
+    try {
+      let res = await axios.post(
+        "https://api-rest-gestmorfi.herokuapp.com/api/orders",
+        orderPayload
+      );
+      ShowToast(res.status, "Orden generada correctamente");
+      setButtonLoading(false);
+    } catch (error) {
+      ShowToast("error", "Ocurrió un error.", "Intente nuevamente, por favor.");
+      setButtonLoading(false);
+    }
+
+    //Unset values
+    setTotal(0);
+    setOrderData({ name: "", additional: "" });
+    inputNameRef.current.value = "";
+    inputAddRef.current.value = "";
+
+    return;
   };
 
-  const SendToast = (status, title, description) => {
-    toast({
-      title: title,
-      description: description,
-      status: status ? status : "success",
-      duration: 9000,
-      isClosable: true,
-    });
-    return;
+  const handleCheckboxChange = (checkbox) => {
+    let currentPrice = parseInt(total);
+    const price = parseInt(
+      meals
+        .filter((meal) => meal._id === checkbox.value)
+        .map((result) => result.price)
+    );
+
+    if (checkbox.checked) {
+      currentPrice += price;
+    } else {
+      currentPrice -= price;
+    }
+
+    setTotal(currentPrice);
+  };
+
+  const handleInputChange = (target) => {
+    setOrderData({ ...orderData, [target.name]: target.value });
   };
 
   return (
@@ -113,34 +158,47 @@ const Meals = () => {
                           value={meal._id}
                           className="check"
                           colorScheme="green"
+                          onChange={() =>
+                            handleCheckboxChange(checkboxRef.current[i])
+                          }
                         ></Checkbox>
                       </Td>
                     </Tr>
                   ))}
               </Tbody>
-              <Tfoot>
-                <Tr>
-                  <Th className="total" pt={6}>
-                    Total
-                  </Th>
-                  <Th></Th>
-                  <Th className="total" isNumeric>
-                    $ {total}
-                  </Th>
-                </Tr>
-              </Tfoot>
             </Table>
+            <Flex mt={5}>
+              <Box className="total-title">TOTAL</Box>
+              <Spacer />
+              <Box className="total-price">$ {total}</Box>
+            </Flex>
             <Stack className="stack" spacing={4} alignItems="center" pt={6}>
-              <Input variant="filled" placeholder="Adicionales" w="100%" />
-              <Input variant="filled" placeholder="Tu nombre" w="100%" />
+              <Input
+                ref={(el) => (inputAddRef.current = el)}
+                name="additional"
+                onChange={(e) => handleInputChange(e.target)}
+                variant="filled"
+                placeholder="Opcional adicionales"
+                w="100%"
+              />
+              <Input
+                ref={(el) => (inputNameRef.current = el)}
+                name="name"
+                onChange={(e) => handleInputChange(e.target)}
+                isInvalid={invalidInput}
+                variant="filled"
+                placeholder="Tu nombre"
+                w="100%"
+              />
             </Stack>
             <Button
+              isDisabled={!orderData.name}
               loadingText="Cargando..."
               isLoading={buttonLoading}
-              onClick={checkMeal}
+              onClick={generateOrder}
               className="confirm-button"
               colorScheme="blue"
-              w="30%"
+              w={["100%", "40%", "30%"]}
               mt={8}
             >
               Confirmar
--- src/utils/utilsFunctions.js
+++ src/utils/utilsFunctions.js
@@ -1,9 +1,56 @@
+import { createStandaloneToast } from "@chakra-ui/react";
+
 export const validateCheckbox = (checkboxes) => {
   let result = false;
 
   checkboxes.current.map((checkbox) => {
-    if (checkbox.checked) result = true;
+    if (checkbox.checked) result = checkbox.value;
   });
 
   return result;
+};
+
+export const ShowToast = (status, title, description) => {
+  switch (status) {
+    case 200:
+      status = "success";
+      break;
+    case 201:
+      status = "success";
+      break;
+    case 400:
+      status = "error";
+      break;
+    case 404:
+      status = "error";
+      break;
+    case 500:
+      status = "error";
+      break;
+    case "error":
+      status = "error";
+      break;
+    default:
+      status = "success";
+      break;
+  }
+
+  const toast = createStandaloneToast();
+  toast({
+    title: title,
+    description: description,
+    status: status,
+    duration: 9000,
+    isClosable: true,
+  });
+};
+
+export const cleanData = (checkboxes) => {
+  checkboxes.current.map((checkbox) => {
+    checkbox.checked = false;
+  });
+
+  checkboxes.current.map((checkbox) => {
+    console.log(checkbox.checked);
+  });
 };
